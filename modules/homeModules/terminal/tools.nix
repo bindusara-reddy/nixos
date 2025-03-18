8ade7a8cb1af04ffadddb4327a7be69215b2d343
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-18 17:34:54 +0530

    z

--- modules/homeModules/terminal/tools.nix
+++ modules/homeModules/terminal/tools.nix
@@ -11,9 +11,7 @@
       enable = true;
       strategy = [ "completion" "match_prev_cmd" "history" ];
     }
-    shellAliases = { 
-      ls = "eza"; 
-    };
+ 
     syntaxHighlighting = {
       enable = true;
       highlighters = [ "main" "brackets" "cursor" "root" ];
