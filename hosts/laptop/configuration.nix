ac3fab0349eaf6d388ac40c1f52cee17b8293fee
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-18 14:53:44 +0530

    zsh

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -2,17 +2,8 @@
 
 {
   imports =
-    [ # Mostly system related configuration
-      ../../modules/nixosModules/nvidia.nix # CHANGEME: Remove this line if you don't have an Nvidia GPU
-      ../../modules/nixosModules/audio.nix
-      ../../modules/nixosModules/gnome.nix
-      ../../modules/nixosModules/home-manager.nix
-      ../../modules/nixosModules/nix.nix
-      ../../modules/nixosModules/systemd-boot.nix
-      ../../modules/nixosModules/users.nix
-      ../../modules/nixosModules/utils.nix
-      ../../modules/nixosModules/docker.nix
-      ../../modules/nixosModules/steam.nix
+    [ 
+      ../../modules/nixosModules
 
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
--- modules/homeModules/terminal/tools.nix
+++ modules/homeModules/terminal/tools.nix
@@ -3,7 +3,6 @@
   programs.zoxide.enable = true;
   programs.eza.enable = true;
   programs.zsh.enable = true;
-  environment.pathToLink = [ "/share/zsh" ];
   programs.oh-my-posh.enable = true;
   programs.oh-my-posh.usetheme = "atomic";
 }
