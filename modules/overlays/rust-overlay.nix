bbb675d7eee8ffb20aad8486587f0287825089bb
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-18 18:29:51 +0530

    rust

--- modules/overlays/rust-overlay.nix
+++ modules/overlays/rust-overlay.nix
@@ -1,4 +1,4 @@
-{ pkgs, rust-overlay ... }: {
+{ pkgs, rust-overlay, ... }: {
 
   nixpkgs.overlays = [ rust-overlay.overlays.default ];
   environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
