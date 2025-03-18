d55cd3788923e7ff444ccd56aad21d910c5ea43d
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-18 17:32:38 +0530

    z

--- modules/homeModules/terminal/tools.nix
+++ modules/homeModules/terminal/tools.nix
@@ -2,10 +2,30 @@
   programs.git.enable = true;
   programs.zoxide.enable = true;
   programs.eza.enable = true;
-  programs.kitty.enable = true; 
+  programs.kitty.enable = true;
+
+  programs.zsh = {
+
+    enable = true;
+    autosuggestion = {
+      enable = true;
+      strategy = [ "completion" "match_prev_cmd" "history" ];
+    }
+    shellAliases = { 
+      ls = "eza"; 
+    };
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [ "main" "brackets" "cursor" "root" ];
+    }
+  };
+
   programs.oh-my-posh = {
+
     enable = true;
     useTheme = "atomic";
+
   };
+
   programs.neovim.defaultEditor = true;
 }
--- modules/nixosModules/zsh.nix
+++ modules/nixosModules/zsh.nix
@@ -1,12 +1,6 @@
 {pkgs, ... }: {
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    histSize = 10000;
-  };
+  users.defaultUserShell = [ pkgs.zsh ];
+  environment.pathsToLink = [ "/share/zsh" ];
 
-  users.defaultUserShell =  [ pkgs.zsh ];
 }
