63e1e399aab2566198d9ff456cc208de8662afe5
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-19 21:34:11 +0530

    kitty

--- modules/home/tools.nix
+++ modules/home/tools.nix
@@ -13,13 +13,13 @@
       font_family = "FiraCode Nerd Font Mono";
       font_size = 14.0;
       window_margin_width = 10;
-      background_image = "/home/${username}/Pictures/desert.png";
+      background_image = "/home/${username}/Pictures/starry.jpg";
       background_image_layout = "scaled";
-      background_tint = 0.9;
+      background_tint = 0.7;
       background_tint_gaps = -10.0;
       window_border_width = "1pt";
       active_border_color = "#44ffff";
-      single_window_margin_width = 0;
+      single_window_margin_width = 5;
       enabled_layouts = "Tall, *";
       tab_bar_style = "powerline";
       tab_powerline_style = "slanted";
@@ -49,7 +49,7 @@
   programs.oh-my-posh = {
 
     enable = true;
-    useTheme = "atomic";
+    useTheme = "kushal";
 
   };
 
