6f17665196aa8e162fc7e3bd390323927fa2cd9d
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-20 23:25:22 +0530

    nushell

--- modules/home/tools.nix
+++ modules/home/tools.nix
@@ -35,6 +35,16 @@
     shellAliases = {
       ls = "eza";
     };
+
+    settings = {
+      show_banner = false;
+      completions = {
+       	case_sensitive = false;
+	quick = true;
+	partial = true;
+	algorithm = "prefix";
+      };
+    };
  
   };
 
