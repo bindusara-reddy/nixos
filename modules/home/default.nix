4be73bcdd5b357112c8cd64da00c5933728c4d76
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-24 10:09:51 +0530

    wezterm

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -2,15 +2,16 @@
 
   imports = [
 
+    ./bacon.nix
     ./eza.nix
     ./firefox.nix
     ./git.nix
     ./home-packages.nix
-    ./kitty.nix
     ./neovim.nix
     ./nushell.nix
     ./starship.nix
     ./vscode.nix
+    ./wezterm.nix
     ./zoxide.nix
 
   ];
---
+++ modules/home/wezterm.nix
@@ -0,0 +1,26 @@
+{
+
+  programs.wezterm = {
+    enable = true;
+    extraConfig = 
+      ''
+      return {
+	inactive_pane_hsb = {
+	  saturation = 0.8,
+	  brightness = 0.7,
+	},
+	font = wezterm.font('JetBrains Mono', { weight = 'Regular', italic = false }),
+        font_size = 15.0,
+	line_height = 1.5,
+	adjust_window_size_when_changing_font_size = true,
+	initial_rows = 15,
+	initial_cols = 80,
+	scrollback_lines = 2000,
+        color_scheme = "Batman",
+	use_dead_keys = false,
+	tab_bar_at_bottom = true,
+      }
+      '';
+  };
+
+}
