6ccae37d43196cd8a7b287c1fe7e31c2f8e74119
bindusara-reddy <bindusara1252001@gmail.com>
2025-03-27 14:21:54 +0530

    rustlings

--- modules/core/rust.nix
+++ modules/core/rust.nix
@@ -4,6 +4,7 @@
   nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
   environment.systemPackages = with pkgs; [ 
     ( rust-bin.stable.latest.default.override { extensions = [ "rust-src" "rust-analyzer"]; } )
+    rustlings
   ];
 
 }
